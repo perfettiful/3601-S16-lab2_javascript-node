4b1cf65edb69b92168f0dd26c0ecce966f0ad489
park1369 <park1369@morris.umn.edu>
2016-02-01 18:13:00 -0600

    changed details

--- serverJavascript.js
+++ serverJavascript.js
@@ -9,9 +9,15 @@ exports.calcPetAge = function(int){
     return int*7;
 };
 
-exports.gpaCalc = function(credit, credit2, credit3, grade, grade2, grade3){
-    var c1 = parseInt(credit);
-    var c2 = parseInt(credit2);
-    var c3 = parseInt(credit3);
-    return (((c1 * grade) + (c2 * grade2) + (c3 * grade3)) / (c1 + c2 + c3)).toFixed(3);
+exports.gpaCalc = function(crd1, crd2, crd3, grd1, grd2, grd3){
+    var c1 = parseInt(crd1);
+    var c2 = parseInt(crd2);
+    var c3 = parseInt(crd3);
+
+    var totalCreds = (c1 + c2 + c3).toFixed(3);
+    var grdPts = ((c1 * grd1) + (c2 * grd2) + (c3 * grd3));
+
+    var gpa = grdPts / totalCreds;
+
+    return gpa;
 };
